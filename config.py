364d34ab171a0c3b09109f23e3109b241bc6327f
OJaved <ojaved20@gmail.com>
2020-03-28 15:20:50 -0400

    deleted config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    TEMPLATES_AUTO_RELOAD = True
-    MONGO_URI = 'mongodb://localhost:27017/essentialstracker'
-    MONGO_DBNAME = 'essentialstracker'
